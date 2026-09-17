2d032eeb11c946ae06d5d3035306fc9951ce3b77
Mike <x33575@gmail.com>
2026-09-17 23:48:53 +0400

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -23,8 +23,8 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(
-    `Server is running on http://localhost:${PORT}`
+    `Server is running on port ${PORT}`
   )
 })
